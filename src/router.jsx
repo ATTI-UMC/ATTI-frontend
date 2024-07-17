b3ff1b86a45925fcd7dc08b5ca045db27aed87a4
KimSanghyun20 <sanghyun1201@naver.com>
2024-07-17 22:19:00 +0900

    변경된 폴더구조 적용

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
+import Login from "./pages/Login"
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     path: "/signup",
     element: <SignUp />,
   },
+  {
+    path: "/login",
+    element: <Login />
+  }
 ]);
 
 export default router;
