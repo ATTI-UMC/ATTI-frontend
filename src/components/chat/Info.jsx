c7ccab580c39bf96f262cc701c3cb18c6286cb76
KimSanghyun20 <sanghyun1201@naver.com>
2024-08-08 21:01:26 +0900

    feat: 신고 버튼 클릭시 두번째 하단시트 팝업 구현

--- src/components/chat/Info.jsx
+++ src/components/chat/Info.jsx
@@ -7,10 +7,16 @@ import BottomSheet from "./BottomSheet";
 import TextButton from "./TextButton";
 const Info = () => {
   const [isSheetVisible, setIsSheetVisible] = useState(false);
+  const [isReportSheetVisible, setIsReportSheetVisible] = useState(false);
 
   const toggleSheet = () => {
     setIsSheetVisible(!isSheetVisible);
-  }
+  };
+
+  const toggleReportSheet = () => {
+    setIsReportSheetVisible(!isReportSheetVisible);
+    setIsSheetVisible();
+  };
 
   return(
     <Container>
@@ -23,9 +29,19 @@ const Info = () => {
         <Icon src={ button_report } alt="report" onClick={toggleSheet}/>
         <Icon src={ button_exit } alt="exit"/>
       </IconsWrapper>
+      {/* 첫번째 팝업 */}
       <BottomSheet isVisible={isSheetVisible} onClose={toggleSheet}>
-        <TextButton onClick={() => alert('신고')}>신고</TextButton>
+        <TextButton onClick={(toggleReportSheet)}>신고</TextButton>
         <TextButton onClick={() => alert('차단')}>차단</TextButton>
+      </BottomSheet>
+      {/* 두번째 팝업 */}
+      <BottomSheet isVisible={isReportSheetVisible} onClose={toggleReportSheet}>
+        <Title>어떤 문제가 있나요?</Title>
+        <TextButton onClick={() => alert('상대방이 답장 없음')}>상대방이 답장 없음</TextButton>
+        <TextButton onClick={() => alert('허위 프로필 및 사진 도용')}>허위 프로필 및 사진 도용</TextButton>
+        <TextButton onClick={() => alert('불법 촬영물 공유')}>불법 촬영물 공유</TextButton>
+        <TextButton onClick={() => alert('욕설 및 불쾌한 대화')}>욕설 및 불쾌한 대화</TextButton>
+        <TextButton onClick={() => alert('기타')}>기타</TextButton>
       </BottomSheet>
     </Container>
   )
@@ -70,4 +86,11 @@ const Icon = styled.img`
   width: 16px;
   height: 18px;
   cursor: pointer;
+`
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 1px;
+  text-align: center;
+  padding: 20px;
 `
